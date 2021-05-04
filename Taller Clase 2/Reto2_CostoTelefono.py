305381f0d8ea82d0e2d89767662874ef1577b551
carolinajacome <carolinajacome14@gmail.com>
2021-05-03 21:59:23 -0500

    Add files via upload

---
+++ Taller Clase 2/Reto2_CostoTelefono.py
@@ -0,0 +1,4 @@
+costotelefono= 420
+precioconIva= costotelefono*1.21
+precioNextDay= precioconIva*1.20
+print("El precio del teléfono con Iva es "+str(precioconIva) +"€" ,"El precio del teléfono el día siguiente será de "+str(precioNextDay)+"€")
